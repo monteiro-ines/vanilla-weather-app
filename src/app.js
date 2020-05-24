2405c4e280d6c854055852785275a07df5565801
monteiro-ines <monteiro-ines@users.noreply.github.com>
2020-05-24 21:56:59 +0100

    added the search engine

--- src/app.js
+++ src/app.js
@@ -34,11 +34,23 @@ function displayTemperature(response){
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "dffc8c427a5c5c2e6c671ca095897824";
-let city = "Porto";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey = "dffc8c427a5c5c2e6c671ca095897824";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit (event){
+event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search ("Porto");
 
 
-axios.get(apiUrl).then(displayTemperature);
+let form= document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
